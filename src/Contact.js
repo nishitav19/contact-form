ca5877eb5d9da9a0b171507cddf682508ffbbbf1
Nishita Vaddem <nishitavaddem@gmail.com>
2021-01-31 11:13:11 +0530

    Add form and css styles

--- src/Contact.js
+++ src/Contact.js
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <div className="contact-form">
       <div className="align-header">
-        <h2>Get in touch!</h2>
+        <h3>Get in touch!</h3>
         <div className="align-icons">
           <i className="fa fa-map-marker" aria-hidden="true"></i>
           <i className="fa fa-phone" aria-hidden="true"></i>
@@ -18,6 +18,21 @@ function Contact() {
           <p>102 Street 5093 Lyon</p>
           <p>+02 1234 567</p>
           <p>support@aotinc.com</p>
+        </div>
+        <div className="form">
+          <form>
+            <label>Name</label>
+            <input type="text" required placeholder="Sarah Jones" />
+            <label>Email</label>
+            <input type="email" required placeholder="sarahjones@gmail.com" />
+            <label>Message</label>
+            <textarea
+              type="text"
+              required
+              placeholder="Describe the message below."
+            ></textarea>
+            <button>Submit</button>
+          </form>
         </div>
       </div>
     </div>
